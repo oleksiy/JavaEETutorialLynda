668672be5504f487695e91cd7875c68b2127211c
Oleksiy Blavat <blavat.alex@gmail.com>
2018-04-12 22:25:00 -0400

    fails to initialize sessionBean

--- springmvcmaven/src/main/java/com/safari/training/common/App.java
+++ springmvcmaven/src/main/java/com/safari/training/common/App.java
@@ -1,4 +1,33 @@
 package com.safari.training.common;
 
+import com.safari.training.stock.bo.StockBo;
+import com.safari.training.stock.model.Stock;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
+    public static void main(String[]args){
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring/config/BeanLocations.xml");
+
+        StockBo stockBusinessObject = (StockBo)applicationContext.getBean("stockBo");
+
+        //insert
+        Stock stock = new Stock();
+        stock.setStockCode("7668");
+        stock.setStockName("TSLA");
+        stockBusinessObject.save(stock);
+
+
+        //select
+        Stock stock2 = stockBusinessObject.findByStockCode("7668");
+        System.out.println(stock2);
+
+        //update
+        stock2.setStockName("AAPL");
+        stockBusinessObject.update(stock2);
+
+        stockBusinessObject.delete(stock2);
+
+        System.out.println("done");
+    }
 }
--- springmvcmaven/src/main/java/com/safari/training/stock/dao/impl/StockDaoImpl.java
+++ springmvcmaven/src/main/java/com/safari/training/stock/dao/impl/StockDaoImpl.java
@@ -1,4 +1,31 @@
 package com.safari.training.stock.dao.impl;
 
-public class StockDaoImpl {
+import com.safari.training.stock.dao.StockDao;
+import com.safari.training.stock.model.Stock;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+import java.util.List;
+
+public class StockDaoImpl extends HibernateDaoSupport implements StockDao {
+
+    @Override
+    public void save(Stock stock) {
+        getHibernateTemplate().save(stock);
+    }
+
+    @Override
+    public void update(Stock stock) {
+        getHibernateTemplate().update(stock);
+    }
+
+    @Override
+    public void delete(Stock stock) {
+        getHibernateTemplate().delete(stock);
+    }
+
+    @Override
+    public Stock findByStockCode(String stockCode) {
+        List list = getHibernateTemplate().find("from stock where stockCode=?", stockCode);
+        return (Stock)list.get(0);
+    }
 }
